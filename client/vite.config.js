2cd9daeeb96febd7685d86b5c9a8af6319fab382
caufieldjh <j.harry.caufield@gmail.com>
2024-06-20 17:40:28 -0400

    Add missing semicolons

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode }) => {
   // https://vitejs.dev/config/#using-environment-variables-in-config
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
 
   return{
     server: {
@@ -21,5 +21,5 @@ export default defineConfig(({command, mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
-  }
-})
+  };
+});
