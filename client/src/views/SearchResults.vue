e1937a02fe5bae1452a5e23eb31c7e14888e3aaf
Hector Plahar <hector.plahar@gmail.com>
2024-08-22 18:04:12 -0700

    issue #45 continued; removing unneeded SearchInput component and updating search results based on discussion at tech meeting

--- client/src/views/SearchResults.vue
+++ client/src/views/SearchResults.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
 import DatasetDataService from "../services/DatasetDataService";
-import {onBeforeMount, watch} from "vue";
+import {onBeforeMount, ref, watch} from "vue";
 
-let results: any = [];
+let results = [];
+let selectedResult = ref();
+selectedResult.value = results[0];
 
 onBeforeMount(() => {
   DatasetDataService.getAll()
       .then(response => {
         results = response.data;
+        if (results && results.length)
+          selectedResult.value = results[0];
       })
       .catch(e => {
         console.error(e);
       })
 })
+
+// user clicks on a result in the left column
+// updates the object whose details are being displayed in the right
+const onSelectResult = (result: any) => {
+  selectedResult.value = result;
+}
 
 const props = defineProps({
   filter: String
@@ -27,47 +37,109 @@ watch(() => props.filter, (value) => {
 </script>
 
 <template>
-  <div class="container-fluid">
-    <div class="row pt-3">
-      <div class="col">
 
-        <div v-if="!results || !results.length">
-          <i>No data available</i>
-        </div>
+  <div class="page-container" v-if="results && results.length">
+    <div class="left-column">
 
-        <table class="table table-responsive table-bordered" v-else>
-          <thead>
-          <tr>
-            <th class="text-muted text-uppercase small">Date</th>
-            <th class="text-muted text-uppercase small">Identifier</th>
-            <th class="text-muted text-uppercase small">Title</th>
-            <th class="text-muted text-uppercase small">BRC</th>
-            <th class="text-muted text-uppercase small">Creator</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="datum in results">
-            <td>{{ datum.date }}</td>
-            <td>
-              <a v-bind:href="datum.bibliographicCitation" target="_blank">{{ datum.identifier }}</a>
-            </td>
-            <td>
-              <div class="text-truncate small">{{ datum.title }}</div>
-            </td>
-            <td>{{ datum.brc }}</td>
-            <td class="small">
-              {{ datum.creator[0].creatorName }}
-              <div>{{ datum.creator[0].email }}</div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
+      <div class="fw-bold text-center small">{{ results.length }} results found</div>
+
+      <div class="list-group">
+        <a href="#" class="list-group-item d-flex justify-content-between list-group-item-action"
+           v-for="result in results"
+           @click="onSelectResult(result)">
+          <div class="ms-2 me-auto">
+            <div class="mb-2 fw-bold">{{ result.identifier }}</div>
+            <div class="small">{{ result.title }}</div>
+          </div>
+          <small style="font-size: 0.75em">{{ result.date }}</small>
+        </a>
       </div>
+
+    </div>
+    <div class="right-column">
+      <h3>{{ selectedResult.title }}</h3>
+      <div>
+        More information at: <a v-bind:href="selectedResult.bibliographicCitation" target="_blank"
+                                class="btn btn-primary btn-sm"><i class="bi bi-box-arrow-up-right"></i>
+        {{ selectedResult.identifier }}</a>
+      </div>
+
+      <div class="mt-4">
+        <b class="small">Date</b><br>
+        {{ selectedResult.date }}
+      </div>
+
+      <div class="mt-4">
+        <b class="small">Creator</b><br>
+        <div>{{ selectedResult.creator[0].creatorName }}</div>
+      </div>
+
+      <div class="mt-4">
+        <b class="small">BRC</b><br>
+        <div>{{ selectedResult.brc }}</div>
+      </div>
+
+      <div class="mt-4">
+        <b class="small">Repository</b><br>
+        <div>{{ selectedResult.repository }}</div>
+      </div>
+
+      <div class="mt-4" v-if="selectedResult.analysisType">
+        <b class="small">Analysis Type</b><br>
+        <div>{{ selectedResult.analysisType }}</div>
+      </div>
+
+      <div class="mt-4">
+        <b class="small">Description</b><br>
+        {{ selectedResult.description }}
+      </div>
+
+      <div class="mt-4" v-if="selectedResult.keywords && selectedResult.keywords.length">
+        <b class="small">Keywords</b><br>
+        {{ Array.from(selectedResult.keywords).join(', ') }}
+      </div>
+
+      <div class="mt-4" v-if="selectedResult.species">
+        <b class="small">Species</b><br>
+        <div class="d-flex justify-content-start">
+
+          <div v-for="species in selectedResult.species">
+            <div class="me-5">
+              <b class="fs-7 text-uppercase text-muted">Scientific Name: </b>{{ species.scientificName }}<br>
+              <b class="fs-7 text-uppercase text-muted">NCBITaxID: </b>{{ species.NCBITaxID }}
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 
 </template>
 
 <style scoped>
+body {
+  margin: 0;
+}
+
+.page-container {
+  display: flex;
+  height: 100vh;
+}
+
+.left-column {
+  width: 400px;
+  overflow-y: scroll;
+  padding: 20px;
+}
+
+.right-column {
+  flex: 1;
+  padding: 20px;
+}
+
+.fs-7 {
+  font-size: 0.7em;
+}
 
 </style>
