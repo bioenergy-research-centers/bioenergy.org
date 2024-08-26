015bb1a123a4a296b5ad6b4bbc880bb159bd8157
Nick Thrower <throwern@msu.edu>
2024-08-26 17:40:29 -0400

    PG based full text search (#60)
    * add postgres full text searching of json objects
    
    * setup multiple filter options and add findByParams method to dataset service
    
    * fix lint errors
    
    * fix remainin lint error

--- api/app/controllers/dataset.controller.js
+++ api/app/controllers/dataset.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const Dataset = db.datasets;
 const Op = db.Sequelize.Op;
+const where = db.Sequelize.where;
 
 // Create and Save a new Dataset
 exports.create = (req, res) => {
@@ -32,13 +33,85 @@ exports.create = (req, res) => {
 
 // Retrieve all Datasets from the database.
 exports.findAll = (req, res) => {
-  const term = req.query.title;
-  const condition = term ? { 'json.title': { [Op.iLike]: `%${term}%` } } : null;
+  const textQueryTerm = req.query.fulltext;
+  const titleQueryTerm = req.query.title;
+  const brcQueryTerm = req.query.brc;
 
-//  const term = req.query.brc;
-//  let condition = term ? { 'json.brc': `${term}` } : null;
+  // Initialize an empty array for search the conditions
+  const conditions = [];
 
-  Dataset.findAll({ where: condition })
+  if (textQueryTerm) {
+    // Setup a search across all JSON data using postgres built-in full text search.
+    // This works okay for matching multiple terms after manually adding a wildcard prefix to support partial matches.
+    // Supporting special characters makes the token processing more complicated.
+    // https://www.postgresql.org/docs/15/textsearch-controls.html#TEXTSEARCH-PARSING-QUERIES
+    // https://www.postgresql.org/docs/15/datatype-textsearch.html#DATATYPE-TSQUERY
+
+    // NOT and OR boolean terms are supported along with characters '!' and '|'
+    // Parenthesis can be used to group and set precedence  term1 and term2 OR (term3 NOT term4)
+
+    // Split the input into terms on whitespace
+    const specialTokens = ['OR', '|', '!', '(', ')'];
+    const searchTerms = textQueryTerm.trim().match(/(\(|\)|\bOR\b|\bNOT\b|[^\s()]+)/gi);
+    const tokenizedSearchTerms = searchTerms.filter(t => t).map(token => {
+      // convert keywords to characters
+      if (token.toUpperCase() == 'OR') {
+        return '|';
+      }
+      if (token.toUpperCase() == 'NOT') {
+        return '!';
+      }
+      if (token == ')') {
+        return ') &';
+      }
+      // don't add wildcard to special tokens
+      if (specialTokens.includes(token.toUpperCase())) {
+        return token;
+      } 
+      else {
+        // Add wildcard for partial matching
+        return `${token}:* &`;
+      }
+    });
+    let tokenizedSearchTerm = tokenizedSearchTerms.join(' ');
+    // remove any extra trailing &. Quick hack to avoid more complicated processing of the terms
+    tokenizedSearchTerm = tokenizedSearchTerm.replace(/\&$/, "").replace(/\&\s+\|/g, "|").replace(/\&\s+\)/g,")");
+    const textSearchQuery = where(
+      db.Sequelize.fn(
+        'to_tsvector',
+        'simple',
+        db.Sequelize.cast(db.Sequelize.col('json'), 'text')
+      ),
+      {
+        [Op.match]: db.Sequelize.fn(
+          'to_tsquery',
+          'simple',
+          tokenizedSearchTerm
+        )
+      }
+    );
+    conditions.push(textSearchQuery);
+  }
+  
+  if (titleQueryTerm) {
+    const titleSearchQuery = {
+      'json.title': { [Op.iLike]: `%${titleQueryTerm}%`}
+    };
+    conditions.push(titleSearchQuery);
+  }
+  if (brcQueryTerm) {
+    const brcSearchQuery = {
+      'json.brc': { [Op.iLike]: `${brcQueryTerm}`} 
+    };
+    conditions.push(brcSearchQuery);
+  }
+
+  // Use Op.and to merge conditions
+  const mergedWhereConditions = conditions.length > 0 ? { [Op.and]: conditions } : {};
+
+  Dataset.findAll({
+    where: mergedWhereConditions,
+  })
     .then(data => {
       res.send(data.map(x => x.json));
     })
