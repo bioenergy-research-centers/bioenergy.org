cdafaa7c74b802a2c89d69a5330591e208dfa156
Charles Parker <ctparker@lbl.gov>
2025-02-06 12:10:14 -0500

    Disabled Sequelize logging to prevent polluting the console log.

--- api/app/models/index.js
+++ api/app/models/index.js
@@ -2,12 +2,11 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    logging: console.log,
+    logging: false, // disable logging; default: console.log
     host: dbConfig.HOST,
     port: dbConfig.PORT,
     dialect: dbConfig.dialect,
     operatorsAliases: "0",
-
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
