25c6ad82f406d5749701387f50571854d64c2aca
Nicholas A. Thrower <throwern@msu.edu>
2025-04-28 17:54:56 -0400

    add schema version based dynamic components to render datasets with support for new v0.1.0 schema and prior v0.0.8 schema

---
+++ client/src/views/datasets/Dataset_0_1_0.vue
@@ -0,0 +1,89 @@
+<script setup>
+  import { computed } from 'vue';
+  const props = defineProps(['selectedResult']);
+  const primaryCreators = computed(() => {
+    const creators = props.selectedResult?.creator
+    if(!Array.isArray(creators)) { return []; }
+    return creators.filter(item => item.primaryContact === true);
+  });
+</script>
+
+<template>
+  <h3>{{ selectedResult.title }}</h3>
+        <div>
+          More information at:
+          <a :href="selectedResult.bibliographicCitation" target="_blank" rel="noopener noreferrer" class="btn btn-primary btn-sm">
+            <i class="bi bi-box-arrow-up-right"></i> {{ selectedResult.identifier }}
+          </a>
+        </div>
+
+        <div class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Date</div>
+          {{ selectedResult.date }}
+        </div>
+
+        <div v-if="primaryCreators.length" class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Creator</div>
+          <div>
+            <template v-for="(creator, index) in primaryCreators">
+              {{ creator.name }}<span v-if="index !== primaryCreators.length - 1">, </span>
+            </template>
+          </div>
+        </div>
+
+        <div class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">BRC</div>
+          <div>{{ selectedResult.brc }}</div>
+        </div>
+
+        <div class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Repository</div>
+          <div>{{ selectedResult.repository }}</div>
+        </div>
+
+        <div v-if="selectedResult.analysisType" class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Analysis Type</div>
+          <div>{{ selectedResult.analysisType }}</div>
+        </div>
+
+        <div v-if="selectedResult.description" class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Description</div>
+          {{ selectedResult.description }}
+        </div>
+
+        <div v-if="selectedResult.keywords && selectedResult.keywords.length" class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Keywords</div>
+          {{ Array.from(selectedResult.keywords).join(', ') }}
+        </div>
+
+        <div v-if="selectedResult.species" class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Species</div>
+          <div class="d-flex justify-content-start">
+            <div v-for="species in selectedResult.species" :key="species.NCBITaxID">
+              <div class="me-5">
+                <b class="fs-7 text-uppercase text-muted">Scientific Name: </b>{{ species.scientificName }}<br>
+                <b class="fs-7 text-uppercase text-muted">NCBITaxID: </b>{{ species.NCBITaxID }}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="selectedResult.relatedItem" class="mt-4">
+          <div class="small text-uppercase mt-5 fw-bold">Related Item</div>
+          <div v-for="item in selectedResult.relatedItem">
+            <div class="text-muted italic mt-2 fw-bold">
+              {{ item.relatedItemType }}
+              <i class="bi bi-box-arrow-up-right"></i>
+            </div>
+            <a :href="item.relatedItemIdentifier" target="_blank" rel="noopener noreferrer">
+              {{ item.title }}
+            </a>
+          </div>
+        </div>
+
+        <div class='row mt-4'>
+          <div class='text-end text-muted small'>
+              Schema Version: {{ selectedResult.schema_version }}<br/>
+          </div>
+        </div>
+</template>
