660aadebbe0ade7966837d2368c1359074146641
Nick Thrower <throwern@msu.edu>
2025-05-16 16:18:20 -0400

    add plasmid table and reusable organism link component (#123)

---
+++ client/src/components/OrganismLink.vue
@@ -0,0 +1,19 @@
+<script setup>
+  const props = defineProps(['organism']);
+</script>
+
+<template>
+  <div v-if="organism">
+    <div v-if="organism.scientificName && organism.NCBITaxID">
+      <a :href="'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=' + organism.NCBITaxID" target="_blank" rel="noopener noreferrer">
+        <nobr>
+          {{ organism.scientificName }}
+          <i class="bi bi-box-arrow-up-right"></i>
+        </nobr>
+      </a>
+    </div>
+    <div v-else-if="organism.scientificName">
+      {{ organism.scientificName}}
+    </div>
+  </div>
+</template>
