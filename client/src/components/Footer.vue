fa7244d26f5556b2b83c1a2d3f5f6c4032fd0df6
Ian Gallardo <gallard3@msu.edu>
2025-11-24 10:06:33 -0500

    add landmark markup, padding to links for bigger hit zones

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -3,34 +3,36 @@
 </script>
 
 <template>
-  <div class="container my-5">
-    <div class="small text-center">
-      
-      InterBRC Data Products Portal 
-      <i class="bi bi-c-circle small text-muted"></i>
-      2025
+  <footer>
+    <div class="container my-5">
+      <div class="small text-center">
+        
+        InterBRC Data Products Portal 
+        <i class="bi bi-c-circle small text-muted"></i>
+        2025
 
-      <span class="fw-light text-muted mx-2">|</span>
-      <a href="https://www.jbei.org/wp-content/uploads/2024/08/BRCFlyer_2024-1.pdf" target="_blank" rel="noopener noreferrer" class="text-muted text-decoration-none ms-1">
-        Bioenergy Research Center Brochure 
-      </a>
+        <span class="fw-light text-muted mx-2">|</span>
+        <a href="https://www.jbei.org/wp-content/uploads/2024/08/BRCFlyer_2024-1.pdf" target="_blank" rel="noopener noreferrer" class="text-muted text-decoration-none ms-1">
+          Bioenergy Research Center Brochure 
+        </a>
 
-      <span class="fw-light text-muted mx-2">|</span>
-      <i class="bi bi-chat text-muted"></i>
-      <router-link :to="{ name: 'contact'}" class="text-muted text-decoration-none ms-1">
-        Contact Us
-      </router-link>
-  
-      <span class="fw-light text-muted mx-2">|</span>
-      <i class="bi bi-code text-muted"></i>
-      <a :href="docs_link" class="text-muted text-decoration-none ms-1" target="_blank" rel="noopener noreferrer">Developer API Docs</a>
+        <span class="fw-light text-muted mx-2">|</span>
+        <i class="bi bi-chat text-muted"></i>
+        <router-link :to="{ name: 'contact'}" class="text-muted text-decoration-none ms-1 py-3">
+          Contact Us
+        </router-link>
+    
+        <span class="fw-light text-muted mx-2">|</span>
+        <i class="bi bi-code text-muted"></i>
+        <a :href="docs_link" class="text-muted text-decoration-none ms-1 py-3" target="_blank" rel="noopener noreferrer">Developer API Docs</a>
 
-      <span class="fw-light text-muted mx-2">|</span>
-      <i class="bi bi-github text-muted"></i>
-      <a href="https://github.com/bioenergy-research-centers/" class="text-muted text-decoration-none ms-1" target="_blank" rel="noopener noreferrer">
-        Code
-      </a>
+        <span class="fw-light text-muted mx-2">|</span>
+        <i class="bi bi-github text-muted"></i>
+        <a href="https://github.com/bioenergy-research-centers/" class="text-muted text-decoration-none ms-1 py-3" target="_blank" rel="noopener noreferrer">
+          Code
+        </a>
 
+      </div>
     </div>
-  </div>
+  </footer>
 </template>
