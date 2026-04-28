3b0532610b1d71aba17e1f866b95e6cbcb0d38ed
Valerie Autumn Skye <12895168+valerie-autumn-skye@users.noreply.github.com>
2026-04-28 10:57:57 -0400

    Switched to parameterized URL construction to properly handle URI-encoding of dataset links. (#206)
    Co-authored-by: Valerie Autumn Skye <valerie-autumn-skye@users.noreply.github.com>

--- client/src/views/DatasetShowView.vue
+++ client/src/views/DatasetShowView.vue
@@ -77,7 +77,7 @@ watch(dataset, (newDataset) => {
               <span class="text-muted">This dataset is also catalogued as:</span>
               <span v-for="(item, index) in relatedDatasets" :key="item.uid">
                 <span v-if="index > 0">, </span>
-                <router-link :to="`/datasets/${item.uid}`">
+                <router-link :to="{ name: 'datasetShow', params: { id: item.uid } }">
                   {{ item.uid }}
                 </router-link>
               </span>
